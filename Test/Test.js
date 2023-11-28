0bdcba8018683dff6239b4baf0e320dbe5fbb803
Oumaima <128989099+Am-umaima@users.noreply.github.com>
2023-11-28 12:50:19 +0100

    Update Test.js

--- Test/Test.js
+++ Test/Test.js
@@ -7,104 +7,26 @@ function Clicked(direction)
 	 console.log("direction +1 = "+direction+" + "+SlidItem);
 }
 
-function ShowQuots(index)
+function ShowQuots(indeQoute)
 { 
-// console.log("index "+index);
+// console.log("indeQoute "+indeQoute);
 	var elementshowy ="";
-	var x = document.getElementsByClassName("MyQuotes");
-	if(index>x.length)
+	var Qoute = document.getElementsByClassName("MyQuotes");
+	if(indeQoute>Qoute.length)
 		{
 			SlidItem=1;
 		}
-		if(index<1)
+		if(indeQoute<1)
 		{
-			SlidItem=x.length;
+			SlidItem=Qoute.length;
 		}
-	for ( var i = 0; i < x.length; i++) 
+	for ( var i = 0; i < Qoute.length; i++) 
 	{
-		x[i].style.display="none";
+		Qoute[i].style.display="none";
 	
 	}
-	x[SlidItem-1].style.display="block";
-	// document.getElementById("reult").value=x[SlidItem-1].innerHTML;
+	Qoute[SlidItem-1].style.display="block";
+	// document.getElementById("reult").value=Qoute[SlidItem-1].innerHTML;
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-	// var slideIndex = 1;
-	// showDivs(slideIndex);
-	// console.log("adare3");
-	// function plusDivs(n) {
-	  // showDivs(slideIndex += n);
-	  // console.log("hey plusddiv"+n);
-	// }
-
-	// function showDivs(n) 
-	// {
-	  // console.log("show div" +n );
-	  // var x = document.getElementsByClassName("mySlides");
-		// if (n > x.length) 
-		// {
-			// slideIndex = 1;
-			// Console.log("xlenf: "+slideIndex);
-		// }
-		// if (n < 1)
-		// {
-			// slideIndex = x.length;
-			// console.log("xlenf2: "+slideIndex);
-		// }
-	  
-		// for ( var i = 0; i < x.length; i++) {
-		// x[i].style.display = "none";  
-		// }
-		// x[slideIndex-1].style.display = "block";  
-	// }
-// });
-// document.addEventListener("DOMContentLoaded", function() {
-  // var slideIndex = 1;
-  // showDivs(slideIndex);
-	// console.log("awaday");
-  // document.querySelector(".display-left").addEventListener("click", function() {
-    // plusDivs(-1);
-  // });
-
-  // document.querySelector(".display-right").addEventListener("click", function() {
-    // plusDivs(1);
-  // });
-// });
